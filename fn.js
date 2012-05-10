9fae4ad86ca0c8dace7eed4941ed054c5adf5acc
Rhsy <rhysbrettbowen@hotmail.com>
2012-05-10 09:29:28 -0700

    include flip and curry

---
+++ fn.js
@@ -0,0 +1,53 @@
+goog.provide('func');
+
+
+/**
+ * @param {Function} fn to curry.
+ * @param {Number=} opt_minLength of parameters for function.
+ * @return {Function} curried function.
+ */
+func.curry = function(fn, opt_minLength) {
+  console.log(arguments);
+  var args = arguments.length > 2 ? [].slice.call(arguments, 2) : [];
+  var curried = function() {
+    var newArgs = goog.array.clone(args);
+    if (!arguments.length)
+      return fn.apply(this, args);
+    var ind = 0;
+    goog.array.forEach(arguments, function(arg) {
+      if (!goog.isDef(arg)) {
+        while (goog.isDef(newArgs[ind++]));
+      } else {
+        while (goog.isDef(newArgs[ind]))
+          ind++;
+        newArgs[ind] = arg;
+      }
+    });
+    var newArgsLength = goog.array.filter(newArgs, function(arg) {
+      return goog.isDef(arg);
+    }).length;
+    if (opt_minLength <= newArgsLength) {
+      return fn.apply(this, newArgs);
+    }
+    return func.curry.apply(null,
+          goog.array.concat(fn, goog.isDef(opt_minLength) ?
+              opt_minLength - newArgsLength : undefined, newArgs));
+  };
+  return curried;
+};
+
+
+/**
+ * @param {Function} fn to flip.
+ * @return {Function} with initial params flipped.
+ */
+func.flip = function(fn) {
+  return function() {
+    var args = [].slice.call(arguments);
+    if (arguments.length > 1)
+      var temp = args[0];
+      args[0] = args[1];
+      args[1] = temp;
+    fn.apply(this, args);
+  };
+};
